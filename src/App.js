a8676d78db3d4e2407e9942db72f4acc3797fdc9
Irene Rojas <irene@smallwondermedia.com>
2019-07-15 17:21:04 -0400

    nested rules experiments

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
+
       <header className="header">
         This is in the header.
         <div className="subHeader">
-            This is in subHeader.
+            This is in the subHeader.
         </div>
       </header>
 
       <div className="firstDiv">
-        This is in firstDiv
+        <p>This is in firstDiv.</p>
       </div>
 
       <div className="secondDiv">
         This is in the secondDiv.
+        <br/>
+        <p>This is the React logo.</p>
+        <br/>
         <img className="logo" src={logo} alt="logo"/>
       </div>
 
